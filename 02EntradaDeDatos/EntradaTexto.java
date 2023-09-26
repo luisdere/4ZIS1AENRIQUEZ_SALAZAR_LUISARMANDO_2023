f57cdb2790dc474a2a7962edc86595f50c4e9bc4
luisdere <l.enriquez.s041@edu.utc.mx>
2023-09-25 20:57:13 -0600

    Entrada de Datos

---
+++ 02EntradaDeDatos/EntradaTexto.java
@@ -0,0 +1,15 @@
+import java.io.*;   
+
+
+class EntradaTexto{
+    public static void main(String[] args) throws IOException {
+         char nombre;
+         System.out.print("por favor ingresa tu nombre            ");
+         nombre = (char)System.in.read();
+         System.out.print("Hola " + nombre + "Bienvenido a tronar wii ");
+    }
+    
+}
+
+    
+   
