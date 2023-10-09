99e6954be5518474efc7fb08d20bc075f11a314d
luisdere <l.enriquez.s041@edu.utc.mx>
2023-10-09 17:44:03 -0600

    Estructuras Control

--- 03EstructurasControl/EstructurasControl.java
+++ 03EstructurasControl/EstructurasControl.java
@@ -4,10 +4,10 @@ class EstructurasControl{
 
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
-        int opcion, numbinario, total, cantidadproducto, num1, edad, cantidad, numerofac;
-        int positivos = 0, negativos = 0;
+        int opcion, numbinario, total, cantidadproducto, num1, edad, cantidad, numerofac, o;
+        int positivos = 0, negativos = 0, figurah = 0;
         double precioAbono, fahrenheit, celsius, kelvin, rankine;
-        float precio, resultado, compra=0;
+        float precio, resultado, compra=0, cant1, cant2, sum;
         String nombreproducto;
         String binario = "";
         char letra;
@@ -150,25 +150,24 @@ class EstructurasControl{
 
                     switch (opcionGeometria) {
                         case 1:
-                            System.out.print("Ingrese la longitud del rectángulo: ");
+                            System.out.print("Ingrese la longitud del rectangulo: ");
                             longitud = entrada.nextDouble();
-                            System.out.print("Ingrese la anchura del rectángulo: ");
+                            System.out.print("Ingrese la anchura del rectangulo: ");
                             anchura = entrada.nextDouble();
                             areaRectangulo = longitud * anchura;
                             perimetroRectangulo = 2 * (longitud + anchura);
                             System.out.println("Area del rectangulo: " + areaRectangulo);
-                            System.out.println("Perímetro del rectángulo: " + perimetroRectangulo);
+                            System.out.println("Perímetro del rectangulo: " + perimetroRectangulo);
                             break;
                         case 2:
-                            System.out.print("Ingrese la base del triángulo: ");
+                            System.out.print("Ingrese la base del triangulo: ");
                             base = entrada.nextDouble();
-                            System.out.print("Ingrese la altura del triángulo: ");
+                            System.out.print("Ingrese la altura del triangulo: ");
                             altura = entrada.nextDouble();
-
-                            areaTriangulo = (base * altura/2); 
-                            perimetroTriangulo = ((base * base) + (altura * altura));
+                            areaTriangulo = 0.5 * base * altura;
+                            perimetroTriangulo = base + 2 * (Math.sqrt((base * base) + (altura * altura)));
                             System.out.println("Area del triangulo: " + areaTriangulo);
-                            System.out.println("Perímetro del triangulo: " + perimetroTriangulo);   
+                            System.out.println("Perímetro del triangulo: " + perimetroTriangulo);
                             break;
                         case 3:
                             System.out.print("Ingrese el radio de la esfera: ");
@@ -190,13 +189,17 @@ class EstructurasControl{
                     }
                     break;
                 case 7:
+                    int t;
+                    for(t=1; t<=10; t++){
+                        System.out.println(t +" "+ (t*10) + " "+ (t*100)+ " "+ (t*1000)+ " ");
+                    }
                     break;
                 case 8:
                     System.out.print("Ingrese un número entero no negativo: ");
                     numerofac = entrada.nextInt();
 
                     if (numerofac < 0) {
-                        System.out.println("El número debe ser no negativo.");
+                        System.out.println("El número debe ser no negativo");
                     } else {
                         long factorial = 1;
 
@@ -208,16 +211,135 @@ class EstructurasControl{
                     }
                     break;
                 case 9:
+                    System.out.println("Cuadro magico");
+                    System.out.println("Ingrese el tamaño del cuadro a pintar");
+                    num1 = entrada.nextInt();
+                    if(num1 >= 1 && num1 <= 20){
+                        for(int c=1; c<=num1; c++){
+                            for (int g=1; g<=num1; g++){
+                                System.out.print("*");
+                            }
+                            System.out.println("");
+                        }
+                    }else{
+                        System.out.println("Ingrese solo numeros entre el 1 y 20: ");
+                    }
+
                     break;
                 case 10:
+                   System.out.println("Ingrese el tamaño de la figura hueca:");
+                             figurah =entrada.nextInt();
+
+                             if( figurah>= 0 && figurah<= 50);
+                                   for(int f=0; f<figurah; f++){
+                                    System.out.print("*");
+
+                                   }
+                                   System.out.println();
+
+                                   for(int f =0; f<figurah-2; f++){
+                                    System.out.print("*");
+                                    for(int g=0; g<figurah-2; g++){
+                                      System.out.print (" ");
+                                    }
+                                    System.out.println("*");
+                                   }
+
+                                    for(int f=0; f<figurah; f++){
+                                    System.out.print("*");
+                                    }
                     break;
                 case 11:
+                    System.out.println("Patrones de Codigo");
+                    System.out.printf("%s%s%s%s%s%s%s%s%n","","","","","","","","");
+                    System.out.printf("%2s%s%s%s%s%s%s%s%n","","","","","","","","");
+                    System.out.printf("%s%s%s%s%s%s%s%s%n","","","","","","","","");
+                    System.out.printf("%2s%s%s%s%s%s%s%s%n","","","","","","","","");
+                    System.out.printf("%s%s%s%s%s%s%s%s%n","","","","","","","","");
+                    System.out.printf("%2s%s%s%s%s%s%s%s%n","","","","","","","","");
+                    System.out.printf("%s%s%s%s%s%s%s%s%n","","","","","","","","");
+                    System.out.printf("%2s%s%s%s%s%s%s%s%n","","","","","","","","");
+
                     break;
                 case 12:
+                    System.out.println("Diamante :)");
+                             final int n9 = 5;
+ 
+                              for ( o = 1; o <= n9; o++)
+                               {
+                              for (int j = o; j < n9; j++) {
+                               System.out.print(' ');
+                              }
+                              for (int k = 1; k < 2*o; k++) {
+                               System.out.print('*');
+                                 }
+                               System.out.print(System.lineSeparator());
+                              }
+                              for ( o = n9 - 1; o >= 1; o--)
+                              {
+                              for (int j = n9; j > o; j--) {
+                                System.out.print(' ');
+                              }
+                              for (int k = 1; k < (o * 2); k++) {
+                               System.out.print('*');
+                              }
+                               System.out.print(System.lineSeparator());
+                              }
+                             break;
+                             case 13:
+                             System.out.println("Bienvenido a la calculadora de Diego");
+                             System.out.println("1. Si deseas hacer una suma.");
+                             System.out.println("2. Si deseas hacer una resta.");
+                             System.out.println("3. Si deseas hacer una multiplicacion.");
+                             System.out.println("4. Si deseas hacer una division.");
+
+                             System.out.print("Ingrese 1, 2, 3 o 4 dependiendo lo que desea elegir:");
+                             int opc= entrada.nextInt();
+
+                             if(opc == 1){
+                             System.out.println("Usted eligio hacer una suma");
+                             System.out.println("Ingrese su primer cantidad o numero: ");
+                             cant1 = entrada.nextFloat();
+                             System.out.println("Ingrese su segunda cantidad o numero: ");
+                             cant2 = entrada.nextFloat();
+                             sum =(cant1 + cant2);
+                             System.out.println("El resultado de su suma es...: " +sum );
+                             break;
+                             }
+
+                             if(opc == 2){
+                             System.out.println("Usted eligio hacer una resta");
+                             System.out.println("Ingrese su primer cantidad o numero: ");
+                             cant1 = entrada.nextFloat();
+                             System.out.println("Ingrese su segunda cantidad o numero: ");
+                             cant2 = entrada.nextFloat();
+                             float rest = (cant1 - cant2);
+                             System.out.println("El resultado de su resta es...: " +rest);
+                             break;
+                             }
+                 
+                             if(opc == 3){
+                             System.out.println("Usted eligio hacer una multiplicacion");
+                             System.out.println("Ingrese su primer cantidad o numero: ");
+                             cant1 = entrada.nextFloat();
+                             System.out.println("Ingrese su segunda cantidad o numero: ");
+                             cant2 = entrada.nextFloat();
+                             float mult = (cant1 * cant2);
+                             System.out.println("El resultado de su multiplicacion es...: " +mult);
+                             break;
+                             }
+
+                             if(opc == 4){
+                             System.out.println("Usted eligio hacer una division");
+                             System.out.println("Ingrese su primer cantidad o numero: ");
+                             cant1 = entrada.nextFloat();
+                             System.out.println("Ingrese su segunda cantidad o numero: ");
+                             cant2 = entrada.nextFloat();
+                             float div = (cant1 / cant2);
+                             System.out.println("El resultado de su division es...: " +div);
+                             break;
+                             }
                     break;
-                case 13:
-                   break;
-            
                 case 14:
                     break;
                 default:
